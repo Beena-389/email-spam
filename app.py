48cfa6e14c53ad953b85ded7e3ed48e3b629dd46
Beena Devi <you@example.com>
2022-06-03 12:16:31 +0530

    initial commit

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ import pickle
 import numpy as np
 import pandas
 
-import string
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
-import nltk
-nltk.download('punkt')
-import nltk
-from nltk.stem.porter import PorterStemmer
-
-ps = PorterStemmer()
 
 app = Flask(__name__)
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
@@ -23,29 +13,6 @@ model = pickle.load(open('model.pkl','rb'))
 def home():
     return render_template("index.html")
 
-def transform_text(text):
-    text=' '.join(text)
-    text = text.lower()
-    text = nltk.word_tokenize(text)
-
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if (i not in stopwords.words('english')) and (i not in string.punctuation):
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-    return " ".join(y)
 
 
 
@@ -56,15 +23,14 @@ def transform_text(text):
 def predict():
     # receive the values send by user in three text boxes thru request object -> requesst.form.values()
     
-    input_sms = [x for x in request.form.values()]
+    input_mail = [x for x in request.form.values()]
     
-    transformed_sms = transform_text(input_sms)
-    vector_input = tfidf.transform([transformed_sms])
-    result = model.predict(vector_input)[0]
-    if result==0:
-        return render_template('index.html', pred='Not-Spam Email')
-    elif result==1:
+    input_data_features = tfidf.transform(input_mail)
+    result = model.predict(input_data_features)
+    if result==[0]:
         return render_template('index.html', pred='Spam Email')
+    elif result==[1]:
+        return render_template('index.html', pred='Not-Spam Email')
     
 if __name__ == '__main__':
     app.run(debug=False)
